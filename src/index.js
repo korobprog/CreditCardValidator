b7f7d6a2c7a3ca7cd2accf07966c375ab06a09c1
korobprog <korobprog@gmail.com>
2023-09-01 10:08:02 +1000

    finish

--- src/index.js
+++ src/index.js
@@ -2,15 +2,35 @@ import './css/styles.css';
 import { CreditCardValidatorNumber } from "./js/CreditCardValidatorNumber";
 import cardNumberFormats from "./js/data"
 
+import amexMono from './image/mono/amex.svg';
+import visaMono from './image/mono/visa.svg';
+import mastercardMono from './image/mono/mastercard.svg';
+import discoverMono from './image/mono/discover.svg';
+import jcbMono from './image/mono/jcb.svg';
+import dinersMono from './image/mono/diners.svg';
+import mirMono from './image/mono/mir.svg';
+
 const data = cardNumberFormats.cardNumberFormats
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const image = document.querySelector('.image');
+
+image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image" src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${dinersMono}"> 
+   <image class="image" src="${mirMono}"> 
+   
+   `
+
 
 input.addEventListener("change", (event) => {
    const value = event.target.value
-
    button.addEventListener('click', (event) => {
-      event.preventDefault
+      event.preventDefault()
       const cardNumber = data.find(function (item) {
          const result = item.startsWith.some((num) => {
             return value.startsWith(num);
@@ -22,8 +42,96 @@ input.addEventListener("change", (event) => {
       });
       console.log("🚀 ~ file: index.js:23 ~ cardNumber ~ cardNumber:", cardNumber)
       console.log("🚀 ~ file: inputValidation.js:26 ~ cardNumber ~ cardNumber:", cardNumber)
+
+      try {
+         if (cardNumber.title == 'amex') {
+            return (image.innerHTML = `
+   <image class="image" src="${cardNumber.imagecard}">
+   <image class="image" src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${mirMono}"> 
+   `
+            )
+         }
+         if (cardNumber.title == 'visa') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${cardNumber.imagecard}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${mirMono}"> 
+   `)
+         }
+         if (cardNumber.title == 'mastercard') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${visaMono}">
+   <image class="image" src="${cardNumber.imagecard}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${mirMono}"> 
+   `)
+         }
+         if (cardNumber.title == 'discover') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${cardNumber.imagecard}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${mirMono}"> 
+   `)
+         }
+         if (cardNumber.title == 'jcb') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${cardNumber.imagecard}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${mirMono}"> 
+   `)
+         }
+         if (cardNumber.title == 'diners') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${cardNumber.imagecard}">
+   <image class="image" src="${mirMono}"> 
+   `)
+         }
+         if (cardNumber.title == 'mir') {
+            return (image.innerHTML = `
+   <image class="image" src="${amexMono}">
+   <image class="image"src="${visaMono}">
+   <image class="image" src="${mastercardMono}">
+   <image class="image" src="${jcbMono}">
+   <image class="image" src="${discoverMono}">
+   <image class="image" src="${dinersMono}">
+   <image class="image" src="${cardNumber.imagecard}"> 
+   `)
+         }
+
+      } catch (info) {
+         alert("Нет совпадений")
+      }
    });
+
 })
+
+
+
 
 
 
--- src/js/data.js
+++ src/js/data.js
@@ -1,14 +1,10 @@
 import visa from '../image/flat-rounded/visa.svg';
-
 import mastercard from '../image/flat-rounded/mastercard.svg';
-
 import amex from '../image/flat-rounded/amex.svg';
-
 import discover from '../image/flat-rounded/discover.svg';
-
 import jcb from '../image/flat-rounded/jcb.svg';
-
 import diners from '../image/flat-rounded/diners.svg';
+import mir from '../image/flat-rounded/mir.svg';
 
 const cardNumberFormats = [
    { title: 'amex', startsWith: [34, 37], length: [15], imagecard: amex },
@@ -30,23 +26,23 @@ const cardNumberFormats = [
       title: 'discover',
       startsWith: [
          65,
-         611,
          644,
          645,
          646,
          647,
          648,
          649,
+         6011,
          ...Array.from(Array(799).keys(), (x) => x + 622126)
       ],
-      length: [16],
+      length: [16, 17, 18, 19],
       imagecard: discover,
    },
    {
       title: 'jcb',
       startsWith: [...Array.from(Array(61).keys(), (x) => x + 3528)],
       length: [16, 17, 18, 19],
-      imagecard: jcb,
+      imagecard: jcb
    },
    {
       title: 'diners',
@@ -58,12 +54,17 @@ const cardNumberFormats = [
          302,
          303,
          304,
-         305,
-         ...Array.from(Array(61).keys(), (x) => x + 3528)
+         305
       ],
-      length: [14, 15, 16],
+      length: [14, 16],
       imagecard: diners
    },
+   {
+      title: 'mir',
+      startsWith: [2200, 2201, 2202, 2203, 2204],
+      length: [16],
+      imagecard: mir
+   }
 ];
 
 export default { cardNumberFormats };
